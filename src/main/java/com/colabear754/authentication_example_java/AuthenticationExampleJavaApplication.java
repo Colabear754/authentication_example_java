77d5a3ed034caae5aa15227749dbaec4bf4c5938
Colabear754 <jky2406@gmail.com>
2023-05-11 21:29:10 +0900

    Initial commit

---
+++ src/main/java/com/colabear754/authentication_example_java/AuthenticationExampleJavaApplication.java
@@ -0,0 +1,13 @@
+package com.colabear754.authentication_example_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationExampleJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationExampleJavaApplication.class, args);
+    }
+
+}
