9888b13da08aa2450777948fede7e467bb4eb309
Colabear754 <jky2406@gmail.com>
2023-05-21 20:57:16 +0900

    시큐리티 관련 정리

--- src/main/java/com/colabear754/authentication_example_java/security/SecurityConfig.java
+++ src/main/java/com/colabear754/authentication_example_java/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.colabear754.authentication_example_java.security;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -18,18 +17,11 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    private final String[] allowedUrls = {"/", "/swagger-ui/**", "/v3/**"};
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf().disable()
                 .headers(headers -> headers.frameOptions().sameOrigin())
-                .authorizeHttpRequests(requests ->
-                        requests.requestMatchers(allowedUrls).permitAll()
-                                .requestMatchers(PathRequest.toH2Console()).permitAll()
-                                .anyRequest().authenticated()
-                )
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
