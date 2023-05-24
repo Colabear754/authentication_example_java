d2e21731d10aec904a4da422ba89b201fbd5bf4d
Colabear754 <jky2406@gmail.com>
2023-05-24 14:18:14 +0900

    JWT 예외 응답 설정

--- src/main/java/com/colabear754/authentication_example_java/handler/ExceptionResponseHandler.java
+++ src/main/java/com/colabear754/authentication_example_java/handler/ExceptionResponseHandler.java
@@ -1,6 +1,9 @@
 package com.colabear754.authentication_example_java.handler;
 
 import com.colabear754.authentication_example_java.dto.ApiResponse;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
@@ -24,5 +27,20 @@ public class ExceptionResponseHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse> handleUnexpectedException() {
         return ResponseEntity.internalServerError().body(ApiResponse.error("서버에 문제가 발생했습니다."));
+    }
+
+    @ExceptionHandler(SignatureException.class)
+    public ResponseEntity<ApiResponse> handleSignatureException() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiResponse.error("토큰이 유효하지 않습니다."));
+    }
+
+    @ExceptionHandler(MalformedJwtException.class)
+    public ResponseEntity<ApiResponse> handleMalformedJwtException() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiResponse.error("올바르지 않은 토큰입니다."));
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<ApiResponse> handleExpiredJwtException() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiResponse.error("토큰이 만료되었습니다. 다시 로그인해주세요."));
     }
 }
